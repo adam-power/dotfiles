d6194ccfb439ff6e69d6d98aac4c5453a593696e
Adam Power <apower@pivotal.io>
2019-06-28 11:55:21 +0100

    Auto-installing tmux-yank dependency

--- tmux/config.sh
+++ tmux/config.sh
@@ -10,3 +10,22 @@ if [ -d "$tmux_plugins_dir" ]; then
 else
   git clone https://github.com/tmux-plugins/tpm "$tmux_plugins_dir"
 fi
+
+os="$(uname -s)"
+
+case "$os" in
+"Darwin")
+  echo "macOS"
+  if brew outdated | grep -q "reattach-to-user-namespace"; then
+    brew upgrade "reattach-to-user-namespace"
+  else
+    brew install reattach-to-user-namespace
+  fi
+  ;;
+
+*)
+  echo "Unsupported OS: \"${os}\"."
+  echo "You'll need to manually install dependencies for tmux-yank plugin."
+  echo "See the Github page for more info: https://github.com/tmux-plugins/tmux-yank"
+  ;;
+esac
