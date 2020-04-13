22b9ec7d927505153e51aa69c1599c8e25146880
Adam Power <arpower12@gmail.com>
2020-04-13 15:16:07 +0100

    Pull latest base16 code if repo already present

--- zsh/config.sh
+++ zsh/config.sh
@@ -5,4 +5,11 @@ set -eu
 echo "ZSH additional config..."
 
 # Add base16 shell colors
-git clone https://github.com/chriskempson/base16-shell.git ~/.config/base16-shell
+if [ ! -d ~/.config/base16-shell ]; then
+  git clone https://github.com/chriskempson/base16-shell.git ~/.config/base16-shell
+else
+  cwd="$PWD"
+  cd ~/.config/base16-shell
+  git pull
+  cd "$cwd"
+fi
