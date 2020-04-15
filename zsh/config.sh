2036111af84c2afb636c4da41a2f449e44c7577b
Adam Power <arpower12@gmail.com>
2020-04-15 03:49:35 -0700

    Adding oh-my-zsh install to Zsh configuration

--- zsh/config.sh
+++ zsh/config.sh
@@ -4,6 +4,16 @@ set -eu
 
 echo "ZSH additional config..."
 
+# Install oh my zsh
+if [ ! -d ~/.oh-my-zsh ]; then
+  git clone https://github.com/ohmyzsh/ohmyzsh.git ~/.oh-my-zsh
+else
+  cwd="$PWD"
+  cd ~/.oh-my-zsh
+  git pull
+  cd "$cwd"
+fi
+
 # Add base16 shell colors
 if [ ! -d ~/.config/base16-shell ]; then
   git clone https://github.com/chriskempson/base16-shell.git ~/.config/base16-shell
