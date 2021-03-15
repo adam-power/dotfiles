c0e5d861f2bf68817129d234b77a8b28b6177bf4
Adam Power <apower@vmware.com>
2021-03-15 10:49:50 +0000

    Adding macbook home bin directory

--- zsh/macbook.zsh
+++ zsh/macbook.zsh
@@ -12,6 +12,9 @@ alias tree='tree -C'
 # Rust
 export PATH="$HOME/.cargo/bin:$PATH"
 
+# Local bin dir
+export PATH="$HOME/bin:$PATH"
+
 # Kubectl completion
 # Adding || true because this exits with a non-zero code due to a bug
 # See: https://github.com/kubernetes/kubernetes/pull/88165
